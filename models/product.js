0723873541101084430c01c35f9bbe310a913d97
NikoRozo <niko.rozo92@outlook.com>
2019-04-06 18:32:09 -0500

    Actualizar modelo Producto

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const { Model } = require('objection');
 
 class Product extends Model {
     static get tableName() {
-        return 'product'
+        return 'producto'
     }
 
     static get relationMappings() {
