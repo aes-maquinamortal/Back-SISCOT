dcde008318b2a3684bd0482ce920af53004fd3e6
NikoRozo <niko.rozo92@outlook.com>
2019-04-06 22:07:04 -0500

    Data Base

--- db/migrations/20190406195844_create_coti_producto.js
+++ db/migrations/20190406195844_create_coti_producto.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
         knex.schema.createTable('coti_producto', (table) => {
           table.increments('id').primary().unsigned();
           table.integer('cotizacionid').unsigned().references('cotizacion.id');
-          table.integer('productoid',11).unsigned().references('producto.id');
+          table.integer('productoid').unsigned().references('producto.id');
           table.integer('cantidad');
         })
       ])
