774e04818596c1123d18508e0cfee1e0b44364ff
MinMiguelM <cmiguelmg@gmail.com>
2019-04-13 09:52:57 -0500

    query by a list of ids for retrieving products

--- resolvers/product.resolver.js
+++ resolvers/product.resolver.js
@@ -9,6 +9,8 @@ module.exports.Query = {
                 .where('proveedorid', args.proveedorid)
                 .join('producto', 'prov_producto.productoid', '=', 'producto.id')  
                 .select('producto.id', 'producto.nombre', 'producto.referencia', 'producto.url');
+        } else if(args.productIds) {
+            products = await ProductModel.query().findByIds(args.productIds);
         } else {
             products = await ProductModel.query();
         }
